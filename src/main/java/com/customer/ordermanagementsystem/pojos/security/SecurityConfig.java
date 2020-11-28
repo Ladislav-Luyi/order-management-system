4e4213c7a7b059331d07df26cd3173fd9f3aa5d9
Laci <Ladislav.szabo999@gmail.com>
2020-11-28 10:30:58 +0100

    Adjusting security layer

--- src/main/java/com/customer/ordermanagementsystem/pojos/security/SecurityConfig.java
+++ src/main/java/com/customer/ordermanagementsystem/pojos/security/SecurityConfig.java
@@ -1,20 +1,13 @@
 package com.customer.ordermanagementsystem.pojos.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 
-/*
-https://github.com/spring-projects/spring-security-javaconfig/blob/master/samples/preauth/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
-https://stackoverflow.com/questions/22767205/spring-security-exclude-url-patterns-in-security-annotation-configurartion
-https://stackoverflow.com/questions/37285016/what-is-username-and-password-when-starting-spring-boot-with-tomcat
- */
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -26,10 +19,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .authorizeRequests().antMatchers("/otvorene").authenticated()
                         .and()
                         .csrf().disable();
-
-
-
-
     }
-
 }
