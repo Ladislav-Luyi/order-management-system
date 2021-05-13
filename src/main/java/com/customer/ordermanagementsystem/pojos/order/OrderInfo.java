308b1d571b3d68dadc68d501e6a036f8b60f9515
Ladislav-Luyi <ladislav.szabo999@gmail.com>
2021-05-13 14:14:02 +0200

    Fix problem with telephone number

--- src/main/java/com/customer/ordermanagementsystem/pojos/order/OrderInfo.java
+++ src/main/java/com/customer/ordermanagementsystem/pojos/order/OrderInfo.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.stereotype.Component;
 import javax.persistence.Embeddable;
 import javax.persistence.Transient;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -21,7 +22,8 @@ public class OrderInfo {
     private String street;
 
     @NotNull
-    @Size(min=5, max=100, message = "Zadajte prosím telefónne číslo")
+    @Digits(integer=10, fraction=0)
+    @Size(min=10, max=10)
     private String telephoneNumber;
 
     private String payWithCardToDeliveryGuy;
