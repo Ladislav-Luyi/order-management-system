3a34dccf1be0bfbc4c08fb713b406f7e91013a4f
Ladislav-Luyi <ladislav.szabo999@gmail.com>
2022-12-14 15:29:48 +0100

    Add new openning hours

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,13 +8,13 @@ VALUES (1,true, '');
 
 INSERT IGNORE  INTO OPENNING_HOURS  (ID,DESCRIPTION,OPENNING_HOURS, CLOSING_HOURS,MATCHER, PRIORITY)
 VALUES
-(0,'General openning hours',10,20,'',3),
+(0,'General openning hours',11,17,'',3),
 (1,'Pondelok',-1,-1,'1',2),
 (2,'Utorok',-1,-1,'2',2),
 (3,'Streda',-1,-1,'3',2),
 (4,'Štvrtok',-1,-1,'4',2),
 (5,'Piatok',-1,-1,'5',2),
-(6,'Sobota',-1,-1,'6',2),
+(6,'Sobota',10,11,'6',2),
 (7,'Nedeľa',10,11,'7',2);
 
 INSERT IGNORE  INTO OPENNING_HOURS  (ID,DESCRIPTION,OPENNING_HOURS, CLOSING_HOURS,MATCHER, PRIORITY)
