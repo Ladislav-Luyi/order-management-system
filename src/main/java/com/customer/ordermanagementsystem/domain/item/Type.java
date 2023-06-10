2a404a516d0c442b33d066eaf4acaf88d735dae9
Ladislav-Luyi <ladislav.szabo999@gmail.com>
2023-06-10 10:51:31 +0200

    Simplify Type class

--- src/main/java/com/customer/ordermanagementsystem/domain/item/Type.java
+++ src/main/java/com/customer/ordermanagementsystem/domain/item/Type.java
@@ -2,101 +2,11 @@ package com.customer.ordermanagementsystem.domain.item;
 
 
 public enum Type {
-    PIZZA_NORMAL("PIZZA_NORMAL"), PIZZA_BIG("PIZZA_BIG"), PIZZA_SPECIAL("PIZZA_SPECIAL"),
-    POLIEVKA("POLIEVKA"),      SALAT("SALAT"),    OMACKA("OMACKA"),   POCHUTINA("POCHUTINA"),
-    ZEMIAK_SPECIAL("ZEMIAK_SPECIAL"), BAGETA("BAGETA"), CESTOVINA("CESTOVINA"),
-    GURMANSKE("GURMANSKE"), OBALOVANE("OBALOVANE"), FIT("FIT"), RIZOTO("RIZOTO"), SLADKOST("SLADKOST"), MASOVE_JEDLO("MASOVE_JEDLO"),
-    WRAP_FRITTATA("WRAP_FRITTATA"), NIECO_MALE_POD_ZUB("NIECO_MALE_POD_ZUB"),
-    PRILOHA("PRILOHA"), NAPOJ("NAPOJ"), DOPLNOK("DOPLNOK"),
-    PIVO("PIVO"), VINO("VINO"), ALKOHOL("ALKOHOL");
-
-    private String shortName;
-
-    Type(String shortName) {
-        this.shortName = shortName;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
-
-    public static Type fromShortName(String shortName) {
-        switch (shortName) {
-            case "PIZZA_NORMAL":
-                return Type.PIZZA_NORMAL;
-
-            case "PIZZA_BIG":
-                return Type.PIZZA_BIG;
-
-            case "PIZZA_SPECIAL":
-                return Type.PIZZA_SPECIAL;
-
-            case "POLIEVKA":
-                return Type.POLIEVKA;
-
-            case "SALAT":
-                return Type.SALAT;
-
-            case "OMACKA":
-                return Type.OMACKA;
-
-            case "POCHUTINA":
-                return Type.POCHUTINA;
-
-            case "ZEMIAK_SPECIAL":
-                return Type.ZEMIAK_SPECIAL;
-
-            case "BAGETA":
-                return Type.BAGETA;
-
-            case "CESTOVINA":
-                return Type.CESTOVINA;
-
-            case "GURMANSKE":
-                return Type.GURMANSKE;
-
-            case "OBALOVANE":
-                return Type.OBALOVANE;
-
-            case "MASOVE_JEDLO":
-                return Type.MASOVE_JEDLO;
-
-            case "WRAP_FRITTATA":
-                return Type.WRAP_FRITTATA;
-
-            case "NIECO_MALE_POD_ZUB":
-                return Type.NIECO_MALE_POD_ZUB;
-
-            case "FIT":
-                return Type.FIT;
-
-            case "RIZOTO":
-                return Type.RIZOTO;
-
-            case "SLADKOST":
-                return Type.SLADKOST;
-
-            case "PRILOHA":
-                return Type.PRILOHA;
-
-            case "NAPOJ":
-                return Type.NAPOJ;
-
-            case "DOPLNOK":
-                return Type.DOPLNOK;
-
-            case "PIVO":
-                return Type.PIVO;
-
-            case "VINO":
-                return Type.VINO;
-
-            case "ALKOHOL":
-                return Type.ALKOHOL;
-
-            default:
-                throw new IllegalArgumentException("ShortName [" + shortName
-                        + "] not supported.");
-        }
-    }
+    PIZZA_NORMAL, PIZZA_BIG, PIZZA_SPECIAL,
+    POLIEVKA, SALAT, OMACKA, POCHUTINA,
+    ZEMIAK_SPECIAL, BAGETA, CESTOVINA,
+    GURMANSKE, OBALOVANE, FIT, RIZOTO, SLADKOST, MASOVE_JEDLO,
+    WRAP_FRITTATA, NIECO_MALE_POD_ZUB,
+    PRILOHA, NAPOJ, DOPLNOK,
+    PIVO, VINO, ALKOHOL;
 }
