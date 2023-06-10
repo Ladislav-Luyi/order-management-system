927692e4f46afa86612b539f5ae50768f84a4520
Ladislav-Luyi <ladislav.szabo999@gmail.com>
2023-06-10 12:03:18 +0200

    Refactor security config

--- src/main/java/com/customer/ordermanagementsystem/domain/security/SecurityConfig.java
+++ src/main/java/com/customer/ordermanagementsystem/domain/security/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.customer.ordermanagementsystem.domain.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-@Profile("prod")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
