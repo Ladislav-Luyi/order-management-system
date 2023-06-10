fdce32282526d1bd0cad9f545fead9748b6462c4
Ladislav-Luyi <ladislav.szabo999@gmail.com>
2023-06-10 09:12:06 +0200

    Remove not used classes

--- src/main/java/com/customer/ordermanagementsystem/services/OrderMaintenanceService.java
+++
@@ -1,8 +0,0 @@
-package com.customer.ordermanagementsystem.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface OrderMaintenanceService {
-    void removeOlderThanWeekOrder();
-}
