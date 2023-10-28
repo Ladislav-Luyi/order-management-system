1887df8513da0e2a75c9b5d16d91dfda1c6e117f
Szabo <ladislav.szabo01@t-systems.com>
2023-10-28 19:32:36 +0200

    Refactor Item repository

--- src/main/java/com/customer/ordermanagementsystem/repository/ItemRepository.java
+++ src/main/java/com/customer/ordermanagementsystem/repository/ItemRepository.java
@@ -4,9 +4,6 @@ import com.customer.ordermanagementsystem.domain.item.Item;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ItemRepository extends MongoRepository<Item, String> {
-    List<Item> findAll();
 }
